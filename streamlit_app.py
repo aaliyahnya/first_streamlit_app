28e70eb4674456c74d2104329ca281daef205d8b
aaliyahnya <115667219+aaliyahnya@users.noreply.github.com>
2023-06-27 14:24:22 -0500

    Control flow view

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,12 +34,14 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 fruit_choice_a = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice_a)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice_a)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice_a)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
 
